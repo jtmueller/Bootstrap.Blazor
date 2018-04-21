f9554feabbfa5e7cac38e1a4736bc6132329ffe1
Joel Mueller <joel.mueller@gmail.com>
2018-04-20 23:04:02 -0500

    Initial project setup, exploring possibilities.

---
+++ Bootstrap.Blazor/Shared/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bootstrap.Blazor
+{
+    internal static class Util
+    {
+        public static string ClassNames(params string[] names) => String.Join(" ", names).TrimEnd();
+
+        public static string ClassNames(IEnumerable<string> names) => String.Join(" ", names).TrimEnd();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+This library is intended to be a set of Blazor components that wrap Bootstrap 4, similar to https://reactstrap.github.io/
+
+However, as of Blazor 0.2, all the extra `<blazor-component>` elements are preventing things from rendering correctly.
+Shelving for now until a future Blazor release incorporates this PR: https://github.com/aspnet/Blazor/pull/602
